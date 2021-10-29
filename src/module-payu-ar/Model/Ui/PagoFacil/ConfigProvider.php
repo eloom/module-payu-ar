12dc8b7f1f267daa6ea94d6eb0d7edd775513846
dholi <comunidade@dholi.dev>
2021-10-29 13:14:54 -0300

    Payment logos

--- src/module-payu-ar/Model/Ui/PagoFacil/ConfigProvider.php
+++ src/module-payu-ar/Model/Ui/PagoFacil/ConfigProvider.php
@@ -6,7 +6,7 @@
 * @category     elOOm
 * @package      Modulo PayUAr
 * @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
-* @version      1.0.2
+* @version      1.0.3
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
@@ -18,44 +18,53 @@ namespace Eloom\PayUAr\Model\Ui\PagoFacil;
 use Eloom\PayUAr\Gateway\Config\PagoFacil\Config as PagoFacilConfig;
 use Magento\Checkout\Model\ConfigProviderInterface;
 use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\Escaper;
+use Magento\Framework\View\Asset\Repository;
 
 class ConfigProvider implements ConfigProviderInterface {
-	
+
 	const CODE = 'eloom_payments_payu_pagofacil';
-	
+
+	protected $assetRepo;
+
 	private $config;
-	
+
 	private $session;
-	
+
 	protected $escaper;
-	
-	public function __construct(SessionManagerInterface $session,
-	                            \Magento\Framework\Escaper $escaper,
-	                            PagoFacilConfig $pagoFacilConfig) {
+
+	public function __construct(Repository              $assetRepo,
+	                            SessionManagerInterface $session,
+	                            Escaper                 $escaper,
+	                            PagoFacilConfig         $pagoFacilConfig) {
+		$this->assetRepo = $assetRepo;
 		$this->session = $session;
 		$this->escaper = $escaper;
 		$this->config = $pagoFacilConfig;
 	}
-	
+
 	public function getConfig() {
 		$storeId = $this->session->getStoreId();
-		
+
 		$payment = [];
 		$isActive = $this->config->isActive($storeId);
 		if ($isActive) {
 			$payment = [
 				self::CODE => [
 					'isActive' => $isActive,
-					'instructions' => $this->getInstructions($storeId)
+					'instructions' => $this->getInstructions($storeId),
+					'url' => [
+						'logo' => $this->assetRepo->getUrl('Eloom_PayUAr::images/pago-facil.svg')
+					]
 				]
 			];
 		}
-		
+
 		return [
 			'payment' => $payment
 		];
 	}
-	
+
 	protected function getInstructions($storeId): string {
 		return $this->escaper->escapeHtml($this->config->getInstructions($storeId));
 	}
