0b5c23909ea05ff934c81afc3d9b3a7eedf36b6b
dholi <comunidade@dholi.dev>
2022-02-18 23:24:04 -0300

    Currency is dynamically informed by the "Current Currency Code" of the Store Manager.

--- src/module-payu-ar/Model/Ui/RapiPago/ConfigProvider.php
+++ src/module-payu-ar/Model/Ui/RapiPago/ConfigProvider.php
@@ -6,7 +6,7 @@
 * @category     elOOm
 * @package      Modulo PayUAr
 * @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
-* @version      1.0.3
+* @version      1.0.4
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
@@ -17,9 +17,9 @@ namespace Eloom\PayUAr\Model\Ui\RapiPago;
 
 use Eloom\PayUAr\Gateway\Config\RapiPago\Config as RapiPagoConfig;
 use Magento\Checkout\Model\ConfigProviderInterface;
-use Magento\Framework\Session\SessionManagerInterface;
-use Magento\Framework\View\Asset\Repository;
 use Magento\Framework\Escaper;
+use Magento\Framework\View\Asset\Repository;
+use Magento\Store\Model\StoreManagerInterface;
 
 class ConfigProvider implements ConfigProviderInterface {
 
@@ -29,26 +29,35 @@ class ConfigProvider implements ConfigProviderInterface {
 
 	private $config;
 
-	private $session;
+	protected $storeManager;
 
 	protected $escaper;
 
 	public function __construct(Repository              $assetRepo,
-	                            SessionManagerInterface $session,
 	                            Escaper                 $escaper,
-	                            RapiPagoConfig          $rapiPagoConfig) {
+	                            RapiPagoConfig          $rapiPagoConfig,
+	                            StoreManagerInterface $storeManager) {
 		$this->assetRepo = $assetRepo;
-		$this->session = $session;
 		$this->escaper = $escaper;
 		$this->config = $rapiPagoConfig;
+		$this->storeManager = $storeManager;
 	}
 
 	public function getConfig() {
-		$storeId = $this->session->getStoreId();
-
+		$store = $this->storeManager->getStore();
 		$payment = [];
+		$storeId = $store->getStoreId();
 		$isActive = $this->config->isActive($storeId);
 		if ($isActive) {
+			$currency = $store->getCurrentCurrencyCode();
+			if ('ARS' != $currency) {
+				return ['payment' => [
+					self::CODE => [
+						'message' =>  sprintf("Currency %s not supported.", $currency)
+					]
+				]];
+			}
+
 			$payment = [
 				self::CODE => [
 					'isActive' => $isActive,
